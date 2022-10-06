f938e50fb6073b57667a02bec9cbeb1b197ed6d8
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-06 03:42:05 -0700

    added color attachemnt based on time

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
+
+function checkTime() {
+    if ((moment().hours() == 9))
+        $("#color9").attr("class", "col-md-10 present")
+    if ((moment().hours() == 10))
+        $("#color10").attr("class", "col-md-10 present")
+    if ((moment().hours() == 11))
+        $("#color11").attr("class", "col-md-10 present")
+    if ((moment().hours() == 12))
+        $("#color12").attr("class", "col-md-10 present")
+    if ((moment().hours() == 13))
+        $("#color13").attr("class", "col-md-10 present")
+    if ((moment().hours() == 14))
+        $("#color14").attr("class", "col-md-10 present")
+    if ((moment().hours() == 15))
+        $("#color15").attr("class", "col-md-10 present")
+    if ((moment().hours() == 16))
+        $("#color16").attr("class", "col-md-10 present")
+    if ((moment().hours() == 17))
+        $("#color17").attr("class", "col-md-10 present")
+
+    if ((moment().hours() < 9))
+        $("#color9").attr("class", "col-md-10 future")
+    if ((moment().hours() < 10))
+        $("#color10").attr("class", "col-md-10 future")
+    if ((moment().hours() < 11))
+        $("#color11").attr("class", "col-md-10 future")
+    if ((moment().hours() < 12))
+        $("#color12").attr("class", "col-md-10 future")
+    if ((moment().hours() < 13))
+        $("#color13").attr("class", "col-md-10 future")
+    if ((moment().hours() < 14))
+        $("#color14").attr("class", "col-md-10 future")
+    if ((moment().hours() < 15))
+        $("#color15").attr("class", "col-md-10 future")
+    if ((moment().hours() < 16))
+        $("#color16").attr("class", "col-md-10 future")
+    if ((moment().hours() < 17))
+        $("#color17").attr("class", "col-md-10 future")
+}
+checkTime();
